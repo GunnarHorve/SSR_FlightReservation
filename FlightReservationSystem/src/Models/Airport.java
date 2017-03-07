b616059f03a3e3a9135568ce4d8f63cae516fcf2
gunnarhorve <gunnarhorve@gmail.com>
2017-03-06 19:49:41 -0500

    finished models && added Data files

--- FlightReservationSystem/src/Models/Airport.java
+++ FlightReservationSystem/src/Models/Airport.java
@@ -5,28 +5,13 @@ package Models;
  * Ideally, this class only holds data queried from server.
  */
 public class Airport {
-	private int gmtOffset;
-	private String code;
-	private String name;
+	public int gmtOffset; // offset for local time, from GMT
+	public String code;   // 3-character airport code
+	public String name;   // full airport name
 	
 	public Airport(String name, String code, int gmtOffset) {
 		this.gmtOffset = gmtOffset;
 		this.code = code;
 		this.name = name;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public String getCode() {
-		return this.code;
-	}
-	
-	/*
-	 * returns local time offset from GMT in hours
-	 */
-	public int getGMTOffset() {
-		return gmtOffset;
 	}
 }
--- FlightReservationSystem/src/Models/Flight.java
+++ FlightReservationSystem/src/Models/Flight.java
@@ -5,5 +5,33 @@ package Models;
  * Ideally, this class only holds data queried from server.
  */
 public class Flight {
-
+	public Airplane type;		// type of airplane
+	public int duration;		// how long the flight is to last (in minutes)
+	public int num;				// UID flight number
+	
+	public Airport dep;			// departing airpot
+	public Airport arr;			// arrival airport
+	
+	public String depTime;		// departure time
+	public String arrTime;		// arrival time
+	
+	public double firstPrice;	// first class ticket price
+	public double coachPrice;	// coach class ticket price
+	
+	public int firstSeats;		// # of already reserved first class seats
+	public int coachSeats;		// # of already reserved coach class seats
+	
+	public Flight(Airplane type, int duration, int num, Airport dep, Airport arr, String depTime, String arrTime, double firstPrice, double coachPrice, int firstSeats, int coachSeats) {
+		this.type = type;
+		this.duration = duration;
+		this.num = num;
+		this.dep = dep;
+		this.arr = arr;
+		this.depTime = depTime;
+		this.arrTime = arrTime;
+		this.firstPrice = firstPrice;
+		this.coachPrice = coachPrice;
+		this.firstSeats = firstSeats;
+		this.coachSeats = coachSeats;
+	}
 }
