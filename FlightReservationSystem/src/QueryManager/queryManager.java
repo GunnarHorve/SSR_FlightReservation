ab090dbb836f3c9e65ce16626fb9cd836b4f8f1f
qiyex <qiyex@130.215.213.242>
2017-03-18 17:36:49 -0400

    queryManager

--- FlightReservationSystem/src/QueryManager/queryManager.java
+++ FlightReservationSystem/src/QueryManager/queryManager.java
@@ -1,11 +1,29 @@
 package QueryManager;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import Models.Airplane;
+import Models.Airport;
 import Models.Flight;
+import XMLParser.XMLParser;
 
 public class queryManager {
+	public static void main(String[] args){
+
+		Airport arrAirport = new Airport("arrAirport","BOS",5);
+		Airport depAirport = new Airport("depAirport","CVG",5);
+		List<Flight> flights = getFlights(arrAirport,depAirport,"2017 May 10 00:54 GMT");
+		for(Flight flight : flights){
+			System.out.println(flight.toString());
+		}
+		
+		
+		
+	}
+	
+	
 	public static List<Flight> requestFlights(){
 		List<Flight> lists = new ArrayList<>();
 		
@@ -18,6 +36,49 @@ public class queryManager {
 	public static void reserveFlights(ArrayList<Flight> lists){
 		
 	}
+	
+public static List<Airport> getAllAirports(){
+		File file = new File("src/Data/airports.xml");
+		List<Airport> airports = XMLParser.readAirport(file);
+		return airports;
+	}
+	
+	public static List<Airplane> getAllAirplanes(){
+		File file = new File("src/Data/Airplanes.xml");
+		List<Airplane> airplanes = XMLParser.readAirplane(file);
+		return airplanes;
+	}
+	
+	public static List<Flight> getAllFlights(){
+		File file = new File("src/Data/ArrivingFlights.xml");
+		List<Flight> flights = XMLParser.readFlight(file);
+		File file1 = new File("src/Data/DepartingFlights.xml");
+		List<Flight> flights1 = XMLParser.readFlight(file1);
+		flights.addAll(flights1);
+		return flights;
+	}
+	
+	public static List<Flight> getFlights(Airport arrAirport, Airport depAirport,String date){
+		List<Flight> flights = getAllFlights();
+		List<Flight> selectedFlights = new ArrayList<>();
+		for(Flight flight : flights){
+			try{
+				if(arrAirport.code.equals(flight.arr.code)&&
+				depAirport.code.equals(flight.dep.code)&&
+				date.equals(flight.depTime))
+				selectedFlights.add(flight);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		return selectedFlights;
+	}
+	
+	
+	
+	
+	
+	
 	public boolean lock(Flight flight){
 		return true;
 	}
