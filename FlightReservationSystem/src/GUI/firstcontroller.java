1360ad3d211af8ed101a38120c56f9857ca9ae6c
Dell <Dell>
2017-04-17 06:04:23 -0400

    'javafx'

--- FlightReservationSystem/src/GUI/firstcontroller.java
+++ FlightReservationSystem/src/GUI/firstcontroller.java
@@ -1,20 +1,34 @@
 package GUI;
 
-import java.awt.EventQueue;
-import java.awt.FlowLayout;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JScrollPane;
-
-import Models.Flight;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
-public class result extends JFrame {
-	public result(){}
-	public result(List<Flight> flights) {
+public class firstcontroller{
+	@FXML RadioButton first;
+	@FXML RadioButton coach;
+	@FXML RadioButton oneway;
+	@FXML RadioButton roundway;
+	@FXML ComboBox depart;
+	@FXML ComboBox arrive;
+	
+	
+		
+
+
+
+	@FXML
+	public void initialize() {
+		// TODO Auto-generated method stub
 		String[] airports = {"Please Choose from Below","Hartsfield-Jackson Atlanta International","Ted Stevens Anchorage International Airport","Austin-Bergstrom International","Baltimore/Washington International",
 				"Logan International","Charlotte Douglas International","Chicago Midway Airport","Chicago O'Hare International","Cincinnati/Northern Kentucky International","Cleveland Hopkins International",
 				"Port Columbus International","Dallas/Ft. Worth International - DFW Airport","Denver International Airport","Detroit Metropolitan Wayne County Airport","Fort Lauderdale/Hollywood International",
@@ -23,46 +37,24 @@ public class result extends JFrame {
 				"John F. Kennedy International","LaGuardia International","Newark Liberty International","Metropolitan Oakland International","Ontario International","Orlando International","Philadelphia International","Sky Harbor International",
 				"Pittsburgh International","Portland International","Raleigh-Durham International","Sacramento International","Salt Lake City International","San Antonio International","Lindbergh Field International","San Francisco International",
 				"Mineta San Jos¨¦ International","John Wayne Airport, Orange County","Seattle-Tacoma International","Lambert-St. Louis International","Tampa International","Dulles International Airport","Ronald Reagan Washington National"};
-		List<String> flig = new ArrayList<>();
-		for (Flight flight:flights){flig.add(flight.toString());}
-		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));;
-		String[] st=new String[flig.size()];
-		String[] a= flig.toArray(st);
-		JScrollPane scrollPane = new JScrollPane();
-		getContentPane().add(scrollPane);
-		setLayout(null);
-		scrollPane.setBounds(50, 50, 400, 200);
-		JList list = new JList(a);
 		
-		list.setLocation(100,100);
-		list.setBounds(50, 50, 200, 300);
-		scrollPane.setViewportView(list);;
+		ObservableList<String> list = FXCollections.observableArrayList(airports);
+		depart.setItems(list);
+		arrive.setItems(list);
+		ToggleGroup group = new ToggleGroup();
+		ToggleGroup group1 = new ToggleGroup();
+		first.setToggleGroup(group);
+		
+
+		coach.setToggleGroup(group);
+		oneway.setToggleGroup(group1);
+		roundway.setToggleGroup(group1);
+		
+
 	}
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					result frame = new result();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
+
+
 
 }
-
-ToggleGroup group = new ToggleGroup();
-ToggleGroup group1 = new ToggleGroup();
-first.setToggleGroup(group);
-coach.setSelected(true);
-
-coach.setToggleGroup(group);
-oneway.setToggleGroup(group1);
-roundway.setToggleGroup(group1);
-oneway.setSelected(true);
